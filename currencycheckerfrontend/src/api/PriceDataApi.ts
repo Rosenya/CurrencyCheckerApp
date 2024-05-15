d2b094498416c0c4fe7c5d0d50aba0ea269982a1
Rosenya <lajluch@gmail.com>
2024-05-15 22:17:50 +0200

    last frontend changes

--- currencycheckerfrontend/src/api/PriceDataApi.ts
+++ currencycheckerfrontend/src/api/PriceDataApi.ts
@@ -3,7 +3,7 @@ import { Price } from "../models/Price";
 
 export class PriceDataApi {
     static getLastPriceForGivenSymbol = async () =>
-      await axios.get<Price[]>("http://localhost:3000/last/{symbol}");
+      await axios.get<Price>("http://localhost:3000/last/{symbol}");
 
     static getLastForAllSymbolsForLastDay = async () =>
         await axios.get<Price[]>("http://localhost:3000/lastday/{symbol}");
--- currencycheckerfrontend/src/api/SymbolDataApi.ts
+++ currencycheckerfrontend/src/api/SymbolDataApi.ts
@@ -4,7 +4,6 @@ import { Symbol } from "../models/Symbol"
 export class SymbolDataApi {
   static getSymbols = async () => 
     await axios.get<Symbol[]>('http://localhost:8080/allSymbols');
-
   
     static postSymbol = async (request: Symbol) =>
       await axios.post<Symbol>("http://localhost:3000//{symbols}/addSymbol", request);
